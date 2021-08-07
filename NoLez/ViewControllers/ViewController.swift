5c1c923dbfd6d1fefa17b90e3ff10769e081216d
Group7 <satveerbhogal@gmail.com>
2021-08-07 22:16:33 +0530

    Initial Commit

---
+++ NoLez/ViewControllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NoLez
+//
+//  Created by SATVEER SINGH on 06/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+   
+    
+    
+}
+
